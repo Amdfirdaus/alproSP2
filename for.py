bbe89d5a095a69139536ff56339c37d002b58561
Amdfirdaus <firdaus2006354@gmail.com>
2025-04-09 01:38:35 +0700

    Add files via upload

---
+++ for.py
@@ -0,0 +1,16 @@
+
+# Angka perkalian
+print ("for in range")
+angka = 3
+
+for i in range(1, 11):
+    print(f"{angka} x {i} = {angka * i}")
+
+print("-"*20)
+print("for in sequence")
+
+nama = ["dia","reni","arga","dina","ridho"]
+
+for i in nama :
+    print(f"nama : {i} ")
+    
---
+++ while.py
@@ -0,0 +1,5 @@
+#menghasilkan bilangan ganjil dengan batas panjang 50 dengan kelipatan 2
+i = 1
+while (i<50):
+    print(i)
+    i = i + 2
